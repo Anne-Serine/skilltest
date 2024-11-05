3ab4d3f886a4814cf6b300c1596fa68128442bbf
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-11-05 23:24:37 +0100

    update Button component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,29 @@
-function Button({ icon, text, id, onClick }) {
-  const primary = "bg-app-primary py-1 px-2 text-white rounded-sm";
+import { Link } from "lucide-react";
 
-  return (
+function Button({ icon, text, id, onClick, type="primary", isActive, url }) {
+  const primary = "bg-app-primary hover:bg-app-header py-1 px-2 text-white rounded-sm min-w-[5rem]";
+  const secondary = "bg-gray-50 border hover:border-app-warn hover:text-app-warn border-black p-1 min-w-[5rem] rounded-sm";
+  const tertiaryBase = "border w-full p-1 flex flex-col gap-1 items-center justify-center text-sm sm:w-[3rem]"
+  const tertiary = isActive ? "bg-gray-200" : "bg-gray-50 hover:bg-gray-300";
+
+  const style =
+    type === "primary"
+      ? primary
+      : type === "secondary"
+      ? secondary
+      : type === "tertiary"
+      ? `${tertiaryBase} ${tertiary}`
+      : primary
+
+  return url ? (
+    <Link to={url} className={style}>
+      {icon}
+      {text}
+    </Link>
+  ) : (
     <button
       value={id}
-      className={primary}
+      className={style}
       id={id}
       onClick={(e) => onClick(e.target.value)}
     >
